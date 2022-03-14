e2aef384b1ece635e96e9391db81ab02c6d77d9b
wangly19 <18607948581@163.com>
2022-03-14 23:27:21 +0800

    feat(renders): renders包引入，修复lint

--- package/renders/src/MobileRender/index.tsx
+++ package/renders/src/MobileRender/index.tsx
@@ -1,28 +1,25 @@
-import * as React from "react";
+import * as React from 'react'
 import { RenderNodeType } from '@moyu-code/schema'
 
 export interface MobileRenderProps {
   materialComponents: Record<string, React.FC | React.ComponentClass>,
   remoteComponents?: string[],
   schema: RenderNodeType[],
-  render?: (element: React.ReactNode, item?: RenderNodeType, index?: number) => React.ReactNode
+  render?: (element: React.ReactNode, item: RenderNodeType, index: number) => React.ReactNode
 }
- 
-const MobileRender: React.FC<MobileRenderProps> = (props) => {
 
-  /** 
+const MobileRender: React.FC<MobileRenderProps> = (props) => {
+  /**
    * 物料组件的Ref缓存层
    * 需要使用者提供当前需要渲染的物料组件集合
    * */
   const materialComponentsRef = React.useRef<any>({})
 
   React.useEffect(() => {
-
     /** Cache component */
     if (props.materialComponents) {
       materialComponentsRef.current = props.materialComponents
     }
-    
   }, [props.materialComponents])
 
   /** 通过schema，渲染组件树 */
@@ -34,32 +31,40 @@ const MobileRender: React.FC<MobileRenderProps> = (props) => {
         if (MaterialComponent) {
           const { children, ...componentProps } = node.props || {}
           if (props.render) {
-            const renderElement = props.render(<MaterialComponent {...componentProps}>
-              {children}
-            </MaterialComponent>, node, componentIndex)
+            const renderElement = props.render(
+              <MaterialComponent {...componentProps}>
+                {children}
+              </MaterialComponent>, node, componentIndex)
             console.log(renderElement, 'renderElement')
             return renderElement
           }
-          return <MaterialComponent {...componentProps} key={node.uid}>
-            {children}
-          </MaterialComponent>
+          return (
+            <MaterialComponent {...componentProps} key={node.uid}>
+              {children}
+            </MaterialComponent>
+          )
         } else {
           console.warn(`Warning: ${node.component}组件未找到，请检查materialComponents是否存在当前组件类型`, materialComponentsRef)
           return null
         }
-      }) 
+      })
     }
-    return <div style={{
-      textAlign: 'center'
-    }}>Not Found</div>
+    return (
+      <div style={{
+        textAlign: 'center'
+      }}
+      >Not Found
+      </div>
+    )
   }, [props.schema, materialComponentsRef.current])
 
-
-  return <div>
-    {
+  return (
+    <div>
+      {
       schemaComponentTree
     }
-  </div>
+    </div>
+  )
 }
 
 export default MobileRender
--- package/renders/tsdx.config.js
+++ package/renders/tsdx.config.js
@@ -1,13 +1,13 @@
-const postcss = require('rollup-plugin-postcss');
- 
+const postcss = require('rollup-plugin-postcss')
+
 module.exports = {
   rollup: (config, options) => {
     config.plugins.push(
       postcss({
         inject: false,
-        extract: !!options.writeMeta,
-      }),
-    );
-    return config;
-  },
-};
+        extract: !!options.writeMeta
+      })
+    )
+    return config
+  }
+}
