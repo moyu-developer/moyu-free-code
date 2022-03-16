050f6bacb0e635b075698891a08d1ca423353fc3
wangly19 <18607948581@163.com>
2022-03-16 22:37:40 +0800

    feat(control): 方法与类型

---
+++ package/control/src/utils/index.ts
@@ -0,0 +1,46 @@
+import { RenderNodeType } from '@moyu-code/schema'
+
+export const findDepSchema = (
+  data: RenderNodeType[],
+  format?: (node: RenderNodeType) => RenderNodeType
+): RenderNodeType[] => {
+  return data.map((item) => {
+    let row = { ...item }
+    if (format) {
+      row = format(item)
+    }
+    return {
+      ...row,
+      children:
+        item.children && item.children.length > 0
+          ? findDepSchema(item.children, format)
+          : undefined
+    }
+  })
+}
+
+/**
+ *
+ * @param tree schemaData
+ * @param some 查找条件
+ * @param path 路径
+ * @returns 路径数组
+ */
+export const findTreePath = (
+  tree: RenderNodeType[],
+  some: (node: RenderNodeType) => boolean,
+  path: number[] = []
+): number[] => {
+  if (!Array.isArray(tree) || tree?.length === 0) return []
+  for (let i = 0; i < tree.length; i++) {
+    const node = tree[i]
+    path.push(i)
+    if (some(node)) return path
+    if (node.children) {
+      const findChildren = findTreePath(node.children, some, path)
+      if (findChildren.length) return findChildren
+    }
+    path.pop()
+  }
+  return []
+}
