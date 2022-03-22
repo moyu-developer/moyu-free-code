0c8bb00808235620192acfbe9bbae4f9d102e11e
wangly19 <18607948581@163.com>
2022-03-22 22:47:18 +0800

    feat(ui): add model

---
+++ package/service/src/common/interceptor/response.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  HttpStatus
+  ,
+  Injectable
+} from '@nestjs/common'
+import type { Observable } from 'rxjs'
+import { map } from 'rxjs'
+import { codeMessage } from '../enums/http'
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  intercept (_: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        code: HttpStatus.CREATED,
+        message: codeMessage[HttpStatus.CREATED]
+      }))
+    )
+  }
+}
