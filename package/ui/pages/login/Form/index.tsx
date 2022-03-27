de58223daf30377586a152e33353ada8be842406
wangly19 <18607948581@163.com>
2022-03-28 01:00:34 +0800

    feat(ui): 登录页面，loginForm 完成 🎉

---
+++ package/ui/pages/login/Form/index.tsx
@@ -0,0 +1,66 @@
+import { useRef } from 'react'
+import { LoginReqDto } from '@/api/auth/login'
+import type { Dispatch, RootState } from '@/common/model'
+import { IconGithubLogo } from '@douyinfe/semi-icons'
+import { Space, Form, Button, Typography } from '@douyinfe/semi-ui'
+import { useRouter } from 'next/router'
+import { useDispatch, useSelector } from 'react-redux'
+import JsCookie from 'js-cookie'
+import styles from './index.module.sass'
+
+const LoginForm = () => {
+  const rememberRef = useRef<Boolean>(Boolean(JsCookie.get('remember')))
+  const dispatch: Dispatch = useDispatch()
+  const isLogin = useSelector((state: RootState) => state.loading.effects.common.loginEffect)
+  const router = useRouter()
+
+  const handleLoginFormSubmit = async (formData: Record<string, any>) => {
+    const { remember, ...user } = formData
+    JsCookie.set('remember', remember)
+    const success = await dispatch.common.loginEffect(user as LoginReqDto)
+    success && router.replace('/')
+  }
+
+  console.log(rememberRef.current, 'rememberRef.current')
+
+  return (
+    <div className={styles.loginForm}>
+      <Form
+        onValueChange={(v) => console.log(v)} onSubmit={handleLoginFormSubmit} initValues={{
+          remember: rememberRef.current
+        }}
+      >
+        <Form.Input
+          field='username'
+          size='large'
+          label='账号'
+          suffix={<Typography.Text>@moyu.dev</Typography.Text>}
+          trigger='blur'
+          placeholder='请输入账号名称'
+          rules={[{ required: true, message: '请输入账号名称???' }]}
+        />
+        <Form.Input
+          mode='password'
+          size='large'
+          field='password'
+          label='密码'
+          trigger='blur'
+          placeholder='请输入当前密码'
+          rules={[{ required: true, message: '请输入当前密码???' }]}
+        />
+        <div className={styles.loginFormRemember}>
+          <Form.Checkbox value={false} field='remember' noLabel>
+            记住我
+          </Form.Checkbox>
+          <Typography.Text link>找回密码?</Typography.Text>
+        </div>
+        <Space vertical className={styles.loginFormButtons}>
+          <Button block htmlType='submit' size='large' theme='solid' type='primary' loading={isLogin}>普通登录</Button>
+          <Button block size='large' style={{ background: '#2D3748' }} theme='solid' type='tertiary' icon={<IconGithubLogo />}>Github 账号登录</Button>
+        </Space>
+      </Form>
+    </div>
+  )
+}
+
+export default LoginForm
--- package/ui/pages/login/index.tsx
+++ package/ui/pages/login/index.tsx
@@ -1,7 +1,45 @@
+import { Col, Row, Nav, Typography, Banner } from '@douyinfe/semi-ui'
+import { IconHorn, IconTick } from '@douyinfe/semi-icons'
 import React from 'react'
+import LoginForm from './Form'
+import styles from './index.module.sass'
 
 export default function Login () {
   return (
-    <div>index</div>
+    <div className={styles.login}>
+      <Nav
+        mode='horizontal'
+        className={styles.loginHeader}
+        header={{
+          logo: <img src='https://sf6-cdn-tos.douyinstatic.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/webcast_logo.svg' />,
+          text: 'Moyu Free'
+        }}
+      />
+      <div className={styles.message}>
+        <Banner
+          type='info'
+          icon={<IconHorn />}
+          closeIcon={<IconTick />}
+          description='A pre-released version is available.'
+        />
+      </div>
+
+      <Row>
+        <Col span={12}>
+          <div className={styles.loginBody}>
+            <img className={styles.loginBodyImage} src='https://www.bing.com/th?id=OHR.SquirrelNesting_EN-US9878096346_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp' />
+          </div>
+        </Col>
+        <Col span={12}>
+          <div className={styles.loginBody}>
+            <div className={styles.loginBodyWelcome}>
+              <Typography.Text>欢迎您，Moyu boys and girls 😊</Typography.Text>
+              <Typography.Title heading={3}>登录你的账号开始</Typography.Title>
+            </div>
+            <LoginForm />
+          </div>
+        </Col>
+      </Row>
+    </div>
   )
 }
