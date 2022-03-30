b88532d2fce6c8c3f669846347652576ed1cf191
wangly19 <18607948581@163.com>
2022-03-30 17:19:19 +0800

    feat(ui): 依赖更新 & request

--- package/ui/common/utils/request.ts
+++ package/ui/common/utils/request.ts
@@ -1,10 +1,20 @@
-import { GotAxios } from '@moyu-code/request'
+import { GotAxios, ComposeFunction } from '@moyu-code/request'
+import jsCookie from 'js-cookie'
+
+const setRequestToken: ComposeFunction = (config) => {
+  config.headers = {
+    ...config.headers,
+    token: jsCookie.get('signAccessToken') || ''
+  }
+  return config
+}
 
 const got = new GotAxios({
   timeout: 3000,
   baseURL: '//localhost:8500/api'
 }, {
-  version: '/v1'
+  version: '/v1',
+  pipe: [setRequestToken]
 })
 
 export default got
