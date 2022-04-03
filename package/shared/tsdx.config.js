533ccb15289cd0173bbde57e1f2e7218573f0684
wangly19 <18607948581@163.com>
2022-04-03 22:52:05 +0800

    feat(shared): 准备迁移shcema包合并，组成composition api

---
+++ package/shared/tsdx.config.js
@@ -0,0 +1,14 @@
+const postcss = require('rollup-plugin-postcss')
+
+module.exports = {
+  rollup: (config, options) => {
+    config.plugins.push(
+      postcss({
+        inject: false,
+        extract: !!options.writeMeta,
+        modules: true
+      })
+    )
+    return config
+  }
+}
