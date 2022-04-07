5d8ec00a887eb622da4a338a6cffc83f1fae4376
wangly19 <18607948581@163.com>
2022-04-07 23:48:00 +0800

    feat(control): 手机壳组件

---
+++ package/control/src/common/components/Screenshot/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Typography } from '@douyinfe/semi-ui'
+import styles from './index.module.sass'
+
+export interface ScreenshotProps {
+  title: string
+}
+
+const Screenshot: React.FC<ScreenshotProps> = (props) => {
+  return (
+    <div
+      className={styles.Mobile}
+    >
+      <div className={styles.MobileTitle}>
+        <Typography.Title heading={6} className={styles.canvasMobileTitle}>{props.title}</Typography.Title>
+      </div>
+      <div
+        className={styles.MobileContent}
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Screenshot
