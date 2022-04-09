898c1e7c3f806becb9f9f7e0f59e7c3ce9a6def4
wangly19 <18607948581@163.com>
2022-04-09 23:03:19 +0800

    chore(control): 手机壳组件，更新semi依赖

--- package/control/src/common/components/Screenshot/index.tsx
+++ package/control/src/common/components/Screenshot/index.tsx
@@ -3,22 +3,18 @@ import { Typography } from '@douyinfe/semi-ui'
 import styles from './index.module.sass'
 
 export interface ScreenshotProps {
-  title: string
+  title: string;
 }
 
 const Screenshot: React.FC<ScreenshotProps> = (props) => {
   return (
-    <div
-      className={styles.Mobile}
-    >
+    <div className={styles.Mobile}>
       <div className={styles.MobileTitle}>
-        <Typography.Title heading={6} className={styles.canvasMobileTitle}>{props.title}</Typography.Title>
+        <Typography.Title heading={6} className={styles.canvasMobileTitle}>
+          {props.title}
+        </Typography.Title>
       </div>
-      <div
-        className={styles.MobileContent}
-      >
-        {props.children}
-      </div>
+      <div className={styles.MobileContent}>{props.children}</div>
     </div>
   )
 }
