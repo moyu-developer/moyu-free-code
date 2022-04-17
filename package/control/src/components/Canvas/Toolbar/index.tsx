efe4f1e4471bfc580efc92f2acfdb9ebf73ece9e
wangly19 <18607948581@163.com>
2022-04-17 23:34:18 +0800

    chore(control): toolbar更新，图标和逻辑

--- package/control/src/components/Canvas/Toolbar/index.tsx
+++ package/control/src/components/Canvas/Toolbar/index.tsx
@@ -3,32 +3,26 @@ import {
   Space,
   Tooltip,
   Modal,
-  Dropdown,
-  SplitButtonGroup,
-  InputGroup,
   InputNumber,
-  Toast
-} from '@douyinfe/semi-ui'
+  message,
+  Input,
+  Typography
+} from 'antd'
 import {
-  IconFilpVertical,
-  IconChevronLeft,
-  IconChevronRight,
-  IconExternalOpen,
+  IconArrowBackUp,
+  IconArrowForwardUp,
+  IconShare,
+  IconPresentationAnalytics,
   IconSend,
-  IconSave,
-  IconDelete,
-  IconMore,
-  IconPhoneStroke,
-  IconMinusCircle,
-  IconPlusCircle,
-  IconArrowLeft,
-  IconVideo
-} from '@douyinfe/semi-icons'
+  IconPlus,
+  IconMinus
+} from '@tabler/icons'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import styles from './index.module.sass'
 import type { Dispatch, RootState } from 'src/common/model'
 import PreviewFrameModal from '../PreviewFrameModal'
+import AntSvg from 'src/common/components/AntSvg'
 
 const Toolbar = () => {
   const scale = useSelector((state: RootState) => state.toolbar.scale)
@@ -36,20 +30,20 @@ const Toolbar = () => {
 
   const Actions = [
     {
-      icon: <IconChevronLeft />,
+      icon: IconArrowBackUp,
       tooltip: '后退',
       itemKey: 'back',
       onClick: () => dispatch({ type: 'SCHEMA_UNDO' })
     },
 
     {
-      icon: <IconChevronRight />,
+      icon: IconArrowForwardUp,
       tooltip: '前进',
       itemKey: 'next',
       onClick: () => dispatch({ type: 'SCHEMA_REDO' })
     },
     {
-      icon: <IconExternalOpen />,
+      icon: IconShare,
       tooltip: '海报分享',
       itemKey: 'share',
       onClick: () => console.log(1)
@@ -59,16 +53,12 @@ const Toolbar = () => {
   /** 处理当前Schema的发布 */
   const handleSchemaPublish = () => {
     Modal.info({
+      type: 'info',
       title: '是否发布？',
       content: '是否要发布当前页面，发布页面后会立即同步并且生效，请谨慎操作。',
-      icon: <IconSend />,
-      cancelButtonProps: { theme: 'borderless' },
-      okButtonProps: {
-        theme: 'solid',
-        onClick: async () => {
-          await dispatch.toolbar.save(1)
-          Toast.success('发布成功')
-        }
+      onOk: async () => {
+        await dispatch.toolbar.save(1)
+        message.success('发布成功')
       }
     })
   }
@@ -76,89 +66,65 @@ const Toolbar = () => {
   return (
     <div className={styles.toolbar}>
       <Space>
-        <Button icon={<IconArrowLeft />} theme='borderless' type='primary'>
-          返回
-        </Button>
+        <Typography.Link>返回</Typography.Link>
       </Space>
-      <Space spacing={0}>
+      <Space size={5}>
         {Actions.map((el) => (
-          <Tooltip key={el.itemKey} content={el.tooltip}>
+          <Tooltip key={el.itemKey} title={el.tooltip}>
             <Button
-              icon={el.icon}
-              theme='borderless'
-              type='primary'
+              icon={<AntSvg icon={el.icon} />}
+              type='link'
+              size='small'
               onClick={() => el.onClick()}
             />
           </Tooltip>
         ))}
-        <InputGroup>
-          <Button
-            theme='borderless'
-            type='primary'
-            disabled={scale === 50}
-            icon={<IconMinusCircle />}
-            onClick={() => dispatch.toolbar.narrow()}
-          />
+
+        <Space size={0}>
+          <Tooltip title='缩小'>
+            <Button
+              type='link'
+              disabled={scale <= 50}
+              icon={<AntSvg icon={IconMinus} />}
+              onClick={() => dispatch.toolbar.narrow()}
+            />
+          </Tooltip>
           <InputNumber
-            value={scale}
-            className={styles.toolbarZoom}
-            hideButtons
             prefix='%'
+            style={{ width: 65 }}
+            value={scale}
+            controls={false}
             min={50}
             max={150}
             defaultValue={100}
-            onNumberChange={(v) => dispatch.toolbar.setScale(Number(v))}
+            onChange={(v) => dispatch.toolbar.setScale(Number(v))}
           />
-          <Button
-            theme='borderless'
-            type='primary'
-            disabled={scale === 150}
-            icon={<IconPlusCircle />}
-            onClick={() => dispatch.toolbar.amplify()}
-          />
-        </InputGroup>
+
+          <Tooltip title='放大'>
+            <Button
+              type='link'
+              disabled={scale >= 150}
+              icon={<AntSvg icon={IconPlus} />}
+              onClick={() => dispatch.toolbar.amplify()}
+            />
+          </Tooltip>
+        </Space>
       </Space>
       <Space>
         <PreviewFrameModal>
-          <Button theme='borderless' type='primary' icon={<IconVideo />} />
-        </PreviewFrameModal>
-        <SplitButtonGroup aria-label='项目操作按钮组'>
           <Button
-            theme='solid'
-            type='primary'
-            style={{ marginRight: 0 }}
-            icon={<IconFilpVertical />}
-            onClick={handleSchemaPublish}
-          >
-            发布
-          </Button>
-          <Dropdown
-            menu={[
-              {
-                node: 'item',
-                name: '预览',
-                icon: <IconPhoneStroke />,
-                onClick: () => {}
-              },
-              {
-                node: 'item',
-                name: '保存',
-                icon: <IconSave />,
-                onClick: () => dispatch.toolbar.save(0)
-              },
-              {
-                node: 'item',
-                name: '删除',
-                type: 'danger',
-                icon: <IconDelete />
-              }
-            ]}
-            trigger='click'
-            position='bottomRight'
-          >
-            <Button type='primary' theme='solid' icon={<IconMore />} />
-          </Dropdown>
-        </SplitButtonGroup>
+            type='link'
+            size='small'
+            icon={<AntSvg icon={IconPresentationAnalytics} />}
+          />
+        </PreviewFrameModal>
+        <Button
+          type='primary'
+          onClick={handleSchemaPublish}
+          icon={<AntSvg icon={IconSend} />}
+        >
+          发布
+        </Button>
       </Space>
     </div>
   )
