ec9fca768022e0abb43d203ddbf2918d0d5f0565
wangly19 <18607948581@163.com>
2022-04-28 23:26:58 +0800

    feat(h5-frame): h5渲染的demo，准备用在预览的时候去用。

--- package/h5-example/next.config.js
+++ package/h5-example/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+
+const withImages = require('next-images')
+
+const withTM = require('next-transpile-modules')([
+  'react-vant',
+]);
+
+const nextConfig = withTM(withImages({
+  // 你项目中其他的 Next.js 配置
+}))
 
 module.exports = nextConfig
