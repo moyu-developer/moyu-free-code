3e428e9c8f0ae2610e4e0e0930dd1175dbdb6409
wangly19 <18607948581@163.com>
2022-05-22 22:15:42 +0800

    chore(client): 移除umi构建产物

--- package/client/src/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/wangly19/Desktop/moyu-developer/moyu-free-code/node_modules/.pnpm/registry.npmmirror.com+@umijs+runtime@3.5.24_react@16.14.0/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- package/client/src/.umi-production/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/wangly19/Desktop/moyu-developer/moyu-free-code/node_modules/.pnpm/registry.npmmirror.com+react-helmet@6.1.0/node_modules/react-helmet';
