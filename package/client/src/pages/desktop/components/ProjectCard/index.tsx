f4e0133ad91ab1c1b9966a9f7874f26efdec0901
wangly19 <18607948581@163.com>
2022-06-16 22:54:11 +0800

    chore(client): projectCard修改

--- package/client/src/pages/desktop/components/ProjectCard/index.tsx
+++ package/client/src/pages/desktop/components/ProjectCard/index.tsx
@@ -1,24 +1,39 @@
 import * as React from 'react'
 import { Link } from 'umi'
-import { Image, Space, Typography, Dropdown, Menu } from 'antd'
+import { Image, Space, Typography, Dropdown, Menu, Badge, BadgeProps } from 'antd'
 import { QueryViewListResponseDto } from '@/api/view/list'
-import { CornerDownLeft } from 'tabler-icons-react'
 import styles from './index.module.sass'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from '@/common/model'
 
-const menu = (
-  <Menu
-    items={[
-      {
-        key: 'delete',
-        label: <Typography.Text type='danger'>删除页面</Typography.Text>
-      }
-    ]}
-  />
-)
+const statusType: Record<number, {text: string; status: BadgeProps['status']}> = {
+  0: {
+    text: '已保存',
+    status: 'processing'
+  },
+  1: {
+    text: '已发布',
+    status: 'success'
+  }
+}
 
 const ProjectCard: React.FC<QueryViewListResponseDto['data']['list'][0]> = (
   props
 ) => {
+  const dispatch: Dispatch = useDispatch()
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: 'delete',
+          label: <Typography.Text type='danger'>删除页面</Typography.Text>,
+          onClick: () => dispatch.myApp.deleteDashViewById(props.id)
+        }
+      ]}
+    />
+  )
+
   return (
     <Dropdown overlay={menu} trigger={['contextMenu']}>
       <Link
@@ -27,7 +42,7 @@ const ProjectCard: React.FC<QueryViewListResponseDto['data']['list'][0]> = (
           query: {
             id: props.id
           }
-        }} component={Typography.Link}
+        }}
       >
         <div className={styles.card}>
           <div className={styles.cardCover}>
@@ -45,16 +60,15 @@ const ProjectCard: React.FC<QueryViewListResponseDto['data']['list'][0]> = (
                 className={styles.cardUserName}
                 ellipsis
               >
-                {props.name}
+                <Space>
+                  <Badge size='small' status={statusType[props.status]?.status} text={statusType[props.status]?.text} />
+                  <span>{props.name}</span>
+                </Space>
               </Typography.Text>
               <Typography.Paragraph type='secondary' className={styles.cardUserDesc} ellipsis={{ rows: 2 }}>
-
                 {props.description || '这家伙很懒，什么东西都没有留下 🎉'}
               </Typography.Paragraph>
             </Space>
-            <div className={styles.cardArrow}>
-              {/* <CornerDownLeft style={{ width: 24, height: 24 }} /> */}
-            </div>
           </div>
         </div>
       </Link>
