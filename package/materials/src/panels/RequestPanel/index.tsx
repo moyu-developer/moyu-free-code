64f21a5aa0618efbb20e81a9f91c065730c6654d
wangly19 <18607948581@163.com>
2022-08-01 23:57:14 +0800

    chore(material): requestPanel修改

--- package/materials/src/panels/RequestPanel/index.tsx
+++ package/materials/src/panels/RequestPanel/index.tsx
@@ -1,35 +1,62 @@
-import * as React from 'react'
-import { Collapse, Form, Input, Radio } from 'antd'
-import BaseUrlSetter from './BaseUrlSetter'
+import * as React from "react";
+import { Collapse, Form, Input, Radio, Row, Col, Select, Button } from "antd";
+import { CodePlus } from "tabler-icons-react";
+import BaseUrlSetter from "./BaseUrlSetter";
 
-const RequestOptions = ['POST', 'GET', 'DELETE', 'PUT'].map(v => ({ label: v, value: v }))
+const RequestOptions = ["POST", "GET", "DELETE", "PUT"].map((v) => ({
+  label: v,
+  value: v,
+}));
 
-export const key = 'RequestPanel'
+export const key = "RequestPanel";
 
 export const render = React.memo((props) => (
-  <Collapse.Panel {...props} header='Request' key={key}>
+  <Collapse.Panel {...props} header="接口设置" key={key}>
+    <Row gutter={[10, 0]}>
+      <Col span={8}>
+        <Form.Item name={["__request", "method"]} label="请求方式">
+          <Select options={RequestOptions} placeholder="请求方式" />
+        </Form.Item>
+      </Col>
 
-    <Form.Item label='转发地址'>
-      <BaseUrlSetter />
-    </Form.Item>
+      <Col span={16}>
+        <Form.Item name="请求地址" label="请求地址">
+          <Input placeholder="请求地址" />
+        </Form.Item>
+      </Col>
+    </Row>
 
-    <Form.Item name={['__request', 'method']} label='请求方式'>
-      <Radio.Group options={RequestOptions} optionType='button' size='small' defaultValue='GET' />
-    </Form.Item>
-
-    <Form.List name={['__request', 'payload']}>
-      {fields =>
-        fields.map(field => (
-          <React.Fragment key={field.key}>
-            <Form.Item name={[field.name, 'key']}>
-              <Input />
-            </Form.Item>
-            <Form.Item name={[field.name, 'value']}>
-              <Input />
-            </Form.Item>
-          </React.Fragment>
-        ))}
+    <Form.List
+      name={["request", "payload"]}
+      initialValue={[{ key: 1, value: 1 }]}
+    >
+      {(fields, { add }) =>
+        fields.map((field, index) => (
+          <Row key={field.key} gutter={[20, 0]}>
+            <Col span={10}>
+              {console.log(fields, 'fields')}
+              <Form.Item name={[field.name, "key"]}>
+                <Input placeholder={`参数${index}`} />
+              </Form.Item>
+            </Col>
+            <Col span={10}>
+              <Form.Item name={[field.name, "value"]}>
+                <Input placeholder={`值${index}, bind参数`} />
+              </Form.Item>
+            </Col>
+            <Col span={4}>
+              <Button
+                type="primary"
+                icon={
+                  <span className="anticon anticon-copy" onClick={add}>
+                    <CodePlus />
+                  </span>
+                }
+              ></Button>
+            </Col>
+          </Row>
+        ))
+      }
     </Form.List>
-
   </Collapse.Panel>
-))
+));
